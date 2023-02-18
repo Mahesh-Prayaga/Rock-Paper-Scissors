4c5692ce34fb864efac4c3189778bddf15fb9c2b
MaheshPrayaga <maheshprayaga135@gmail.com>
2023-02-18 16:48:53 +0530

    Added a little UI

--- external.js
+++ external.js
@@ -1,69 +1,87 @@
+const rock = document.querySelector("#rock-button");
+const paper = document.querySelector("#paper-button");
+const Scissors = document.querySelector("#scissors-button");
+let pChoice = document.querySelector(".p-choice");
+let cChoice = document.querySelector(".c-choice");
+let getValue;
+let playerChoice;
+let playerScore = 0;
+let computerScore = 0;
+let res = document.querySelector(".result");
+const div = document.querySelector(".buttons");
+
+function rockSetter() {
+  getValue = 1;
+  playRound(getValue);
+}
+function paperSetter() {
+  getValue = 2;
+  playRound(getValue);
+}
+function scissorsSetter() {
+  getValue = 3;
+  playRound(getValue);
+}
+
 function getComputerChoice() {
   let randomNum = Math.floor(Math.random() * 3 + 1);
   return randomNum;
 }
 
-console.log("The Rock Paper Scissors Game\n");
-console.log("Rules are:\n");
-console.log("1. The First one to reach Score 3 Wins!\n");
-console.log("2. You Should Have Fun!!\n\n");
-let playerScore = 0;
-let computerScore = 0;
-
-function playRound() {
+function playRound(getValue) {
   const computerChoice = getComputerChoice();
-  const getChoice = prompt("Enter Choice");
-  console.log(`You Pulled a ${getChoice}`);
-  let playerChoice;
 
-  if (getChoice == ("rock" || "Rock" || "ROCK")) {
-    playerChoice = 1;
-  } else if (getChoice == ("paper" || "Paper" || "PAPER")) {
-    playerChoice = 2;
+  if (getValue == 1) {
+    playerChoice = "Rock";
+  } else if (getValue == 2) {
+    playerChoice = "Paper";
   } else {
-    playerChoice = 3;
+    playerChoice = "Scissors";
   }
+
+  pChoice.innerText = `You Pulled a ${playerChoice}`;
 
   if (computerChoice == 1) {
-    console.log(`Computer Pulled a Rock`);
+    cChoice.innerText = `Computer Pulled a Rock`;
   } else if (computerChoice == 2) {
-    console.log(`Computer Pulled a Paper`);
+    cChoice.innerText = `Computer Pulled a Paper`;
   } else {
-    console.log(`Computer Pulled a Scissors`);
+    cChoice.innerText = `Computer Pulled a Scissors`;
   }
 
-  if (computerChoice == playerChoice) {
-    console.log("It's a Draw!");
-  } else if (computerChoice == 1 && playerChoice == 2) {
-    console.log("You Won!, Computer Lost!");
+  if (computerChoice == getValue) {
+    res.innerText = "It's a Draw!";
+  } else if (computerChoice == 1 && getValue == 2) {
+    res.innerText = "You Won!, Computer Lost!";
     playerScore++;
-  } else if (computerChoice == 1 && playerChoice == 3) {
-    console.log("You Lost!, Computer Won!");
+  } else if (computerChoice == 1 && getValue == 3) {
+    res.innerText = "You Lost!, Computer Won!";
     computerScore++;
-  } else if (computerChoice == 2 && playerChoice == 1) {
-    console.log("You Lost!, Computer Won!");
+  } else if (computerChoice == 2 && getValue == 1) {
+    res.innerText = "You Lost!, Computer Won!";
     computerScore++;
-  } else if (computerChoice == 2 && playerChoice == 3) {
-    console.log("You Won!, Computer Lost!");
+  } else if (computerChoice == 2 && getValue == 3) {
+    res.innerText = "You Won!, Computer Lost!";
     playerScore++;
-  } else if (computerChoice == 3 && playerChoice == 1) {
-    console.log("You Won!, Computer Lost!");
+  } else if (computerChoice == 3 && getValue == 1) {
+    res.innerText = "You Won!, Computer Lost!";
     playerScore++;
-  } else if (computerChoice == 3 && playerChoice == 2) {
-    console.log("You Lost!, Computer Won!");
+  } else if (computerChoice == 3 && getValue == 2) {
+    res.innerText = "You Lost!, Computer Won!";
     computerScore++;
   }
 }
 
-for (let round = 1; (computerScore || playerScore) != 3; round++) {
-  console.log(`\nRound ${round}\n`);
-  playRound();
-  console.log(`Your Score: ${playerScore}`);
-  console.log(`Computer Score: ${computerScore}\n`);
-}
-
-if (computerScore > playerScore) {
-  console.log("\n\nComputer Won!");
-} else {
-  console.log("\n\nYou Won!");
+function game() {
+  for (let round = 1; (computerScore || playerScore) != 3; round++) {
+    console.log(`\nRound ${round}\n`);
+    playRound();
+    console.log(`Your Score: ${playerScore}`);
+    console.log(`Computer Score: ${computerScore}\n`);
+  }
+  if (computerScore > playerScore) {
+    console.log("\n\nComputer Won!");
+  } else {
+    console.log("\n\nYou Won!");
+  }
 }
